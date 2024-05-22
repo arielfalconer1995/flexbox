31a4d0ca7b5b816dbc288936f54e99c2462ca6d0
abenitez <abenitez@solnik.com.ar>
2024-05-22 15:58:51 -0300

    sumo cambios al proyecto

---
+++ clase01.js
@@ -0,0 +1,30 @@
+// ES5
+var nombre = "Juan";
+//ES5 no se utiliza más. No se deben declarar variables con la propiedad var.
+
+//ES6
+let apellido = "Martinez"
+const CURSO = "JavaScript"
+
+//Nombrar variables
+//Esta es la manera correcta de nombrar variables en JavaScript. JavaScript va a utilizar la comparación entre mayusculas y minusculas para verificar si las variables son iguales. Por eso hay que tener cuidado con la declaracion de variables al usar minusculas y mayusculas.
+
+//Correcto
+let numero;
+let precioProducto;
+let anioNacimiento;
+
+//Incorrecto
+let año
+let teléfono móvil
+
+//Tipos de datos
+//Tipado dinamico
+
+//number -> Números
+let edad = 20;
+
+//string -> Cadena de texto
+let edad ="20"
+
+//minuto 49:41
