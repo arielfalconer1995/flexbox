0b004c5828b14c6e3d2c5d6760a4459f89e443cc
abenitez <abenitez@solnik.com.ar>
2024-05-23 16:01:41 -0300

    Subo cambios

---
+++ 01-tipos/01-variables.js
@@ -0,0 +1,76 @@
+// ES5
+// var nombre = "Juan";
+//ES5 no se utiliza más. No se deben declarar variables con la propiedad var.
+
+//ES6
+// let apellido = "Martinez"
+// const CURSO = "JavaScript"
+
+//VARIABLES
+
+// let nombre = "Hola Mundo";
+// console.log(nombre);
+
+//Convenciones para crear variables: UpperCamelCase, camelCase, snake_case
+
+// let NombreCompleto; -> UpperCamelCase
+// let nombreCompleto; -> camelCase
+// let nombre_completo; -> snake_case
+
+//Correcto
+
+// let numero;
+// let precioProducto;
+// let anioNacimiento;
+
+//Esta es la manera correcta de nombrar variables en JavaScript. JavaScript va a utilizar la comparación entre mayusculas y minusculas para verificar si las variables son iguales. Por eso hay que tener cuidado con la declaracion de variables al usar minusculas y mayusculas.
+//Las variables declaradas con let, no se puede reasignar
+
+//Incorrecto
+// let año
+// let teléfono móvil
+//*****************************************************
+
+//Tipos de datos
+//En JavaScript los tipos de datos se dividen en 2 grandes grupos
+
+//Tipos Primitivos
+
+//number -> Números
+// let edad = 20;
+
+//string -> Cadena de texto
+// let edad ="20";
+
+//boolean -> Representa un valor verdadero ("true") o falso ("false")
+//let isJavaScriptFun = true;
+
+//undefined -> Una variable que ha sido declarada pero no se le ha asignado un valor.
+//let notDefined;
+
+//null -> Representa la ausencia intencional de cualquier valor.
+//let emptyValue = null;
+
+//Symbol -> Representa un valor único y no modificable.
+//let sym = Symbol("description");
+
+//BigInt -> Representa números enteros grande.
+
+//let bigIntValue = 1234567890123456789012345678901234567890n;
+
+//Tipos de Referencia (Objetos)
+//Los tipos de referencia almacenan una referencia a la ubicación de memoria donde se encuentra el objeto. Los objetos en JavaScript son mutables, lo que significa que su contenido puede cambiar. Los tipos de referencia incluyen:
+
+//Object -> Representa un objeto, que es una colección de propiedades y métodos.
+// let person = {
+//     name: "John",
+//     age: 30
+// };
+
+//Array -> Representa una lista ordenada de valores.
+
+//Funciones -> Representa una función.
+
+//Clases -> Representa una clase.
+
+//minuto 49:41 - zoom
